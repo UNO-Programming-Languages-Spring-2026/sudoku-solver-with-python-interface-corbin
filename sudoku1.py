0c8a9f5c78a181aaf5ebca07c8725772f45cf84e
cmaul25 <167263885+cmaul25@users.noreply.github.com>
2026-03-31 12:50:19 -0500

    1a attempt 2

--- sudoku1.py
+++ sudoku1.py
@@ -1,10 +1,11 @@
 import sys, clingo
 class ClingoApp(clingo.application.Application):
     def main(self, ctl, files):
+        ctl.load("sudoku.lp")
         for f in files:
             ctl.load(f)
         if not files:
-            ctl.load("sudoku.lp")
+            ctl.load("-")
         ctl.ground()
         ctl.solve()
 clingo.application.clingo_main(ClingoApp())
